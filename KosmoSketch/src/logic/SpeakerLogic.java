afe97233f4f2310ee3a121f0ba839a3ef126da7f
kim-heetae <rlaxo96@naver.com>
2021-04-14 22:01:22 +0900

    0414   22:01 hit

--- KosmoSketch/src/logic/SpeakerLogic.java
+++ KosmoSketch/src/logic/SpeakerLogic.java
@@ -1,25 +1,30 @@
 package logic;
 
-	import java.awt.*;
-	import java.net.*;
-	import javax.swing.*;
+import java.awt.*;
+import java.net.*;
+import javax.swing.*;
 
+public class SpeakerLogic extends JFrame implements Runnable {
+	//////////////////// 통신에 관련된 전변 추가 //////////////////
+	ServerSocket	server	= null;
+	Socket			client	= null;
+	JTextArea		jta_log	= new JTextArea();
+	JScrollPane		jsp_log	= new JScrollPane(jta_log);
 
-	
-	public class SpeakerLogic  extends JFrame implements Runnable {
-		//////////////////// 통신에 관련된 전변 추가 //////////////////
-		ServerSocket 	server = null;
-		Socket 			client = null;
-		JTextArea       jta_log = new JTextArea();
-		JScrollPane     jsp_log = new JScrollPane(jta_log);
-			
-		public static void main(String[] args) {
-						
-		}
-		public void broadCasting(String msg) {
-			for(SpeakerLogic
+	public static void main(String[] args) {
+
+	}
+
+	public void broadCasting(String msg) {
+//			for(SpeakerLogic
 			
 		
 			
-		}		
+		}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- KosmoSketch/src/qwrqwrqwr/ServerThreadLogic.java
+++ KosmoSketch/src/qwrqwrqwr/ServerThreadLogic.java
@@ -5,7 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.StringTokenizer;
 
-import kosmo.sketch.proj.Protocol;
+import qwrqwrqwr.Protocol;
 
 
 public class ServerThreadLogic extends Thread{
@@ -45,10 +45,13 @@ public class ServerThreadLogic extends Thread{
 					int protocol = Integer.parseInt(st.nextToken());
 					switch (protocol) {
 					case Protocol._READY:
-						/*
-						 * 
-						 */
-//						sl.readyCount++;
+						//sl.readycount를 1씩 증가시킨다.
+						//globalList의 사이즈가 2 이상이고
+						//readycount 가 globalList.size()와 같아질때 게임을 시작
+						//Protocol._START를 사용하여 oos로 ClientThread에게 게임이 시작했음을 알려준다.
+						//게임을 시작하면 dap을 random으로 정하고 Protocol._DAP을 사용하여 oos로 ClientThread에게 알려준다.
+						//타이머를 작동시킨다.
+						 
 						break;
 						
 					case Protocol._NOT_READY:	
@@ -67,11 +70,13 @@ public class ServerThreadLogic extends Thread{
 //						plan1. 그림은 클라에서 ois로 받을때 자신이 questioner라면 Protocol._PAINT로 들어온 msg는 모두 무시한다?
 //						plan2. 아니라면 broadCasting메소드 안에 if문추가해서 보낸 사람은 받지못하게 한다? 면은 다른 case들도 바꿔준다?
 						break;
+					case Protocol._ERASE:
+						
+						break;
 					case Protocol._EXIT:
 //						broadCasting(Protocol._EXIT + "#" + 유저정보 + "#" + nickName);
 //						sl.globalList.remove(클라정보); - nickname이 아닌 클라정보로
-						break;
-						
+						break;		
 					}
 				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
