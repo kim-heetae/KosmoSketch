9573f55787812699426befff379c9869abc1ff27
eunyoung kim <snu9451@naver.com>
2021-04-16 18:35:11 +0900

    2021 04 16 금  18:35

--- KosmoSketch/src/eunTest/SoS.java
+++ KosmoSketch/src/eunTest/SoS.java
@@ -5,9 +5,13 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import javax.swing.JFrame;
+
+import test.project1.Protocol;
 
 // 서버킹임.
 // 10329
@@ -25,17 +29,16 @@ public class SoS extends JFrame implements Runnable {
 	ObjectOutputStream		oos_server				= null;
 	ObjectInputStream		ois_server				= null;
 	WaitRoomServerThread	waitRoomServerThread	= null;
-	// 로그인에 성공하여 대기실에 접속한 클라이언트의 [닉네임]과 [waitRoomServerThread 인스턴스 주소-동기화, broadCastind을 위함]를 담는다.
+	// 로그인에 성공하여 대기실에 접속한 클라이언트의 [닉네임]과 [waitRoomServerThread 인스턴스 주소-동기화, broadCastind을 위함]를 담을 자료구조 선언
 	Map<String, WaitRoomServerThread> clientList	= null;
 
 	// 생성자___________________________________________________________________________________________
 	public SoS() {
-		//서버소켓 생성 - 서버 start
+		// 서버소켓 생성 - 서버 start
 		createServerSocket();
+		clientList = new HashMap<>();
+		// 클라이언트가 입력한 id와 pw를 읽어들이고, 그것들을 가지고 오라클 DB에 접근해 아이디 존재여부, 패스워드 일치여부를 판단해 오는 메소드 호출(또는 클래스 생성)
 		serverView = new SoSView();
-		serverView.jta_sos.append("Server is now running...");
-		serverView.jta_sos.append("Server is now running...");
-		serverView.jta_sos.append("Server is now running...");
 		serverView.jta_sos.append("Server is now running...");
 		
 	}
@@ -58,9 +61,30 @@ public class SoS extends JFrame implements Runnable {
 				waitRoomClientSocket = waitRoomServerSocket.accept();
 				oos_server = new ObjectOutputStream(waitRoomClientSocket.getOutputStream());
 				ois_server = new ObjectInputStream(waitRoomClientSocket.getInputStream());
-				
-			} catch (IOException e) {
-				e.printStackTrace();
+				// 클라이언트가 로그인 버튼을 누를 때 클라이언트쪽에서 Socket이 생성되고 곧바로 입력한 id와 pw 정보를 stream을 통해 서버로 보낸다.
+				// msg: _LOGIN#아이디#패스워드
+				String msg = ois_server.readObject().toString();
+				StringTokenizer st = new StringTokenizer(msg, Protocol._CUT);
+				String id = st.nextToken();
+				String pw = st.nextToken();
+				// 데이터 연동을 위한 DAO 클래스 인스턴스 생성
+				// insert here
+				// DAO 클래스를 통해 로그인 성공 여부(id와 pw 존재, 일치 여부)가 파악되었으면, 그 정보를 클라이언트에게도 알려야 한다.
+				/*	DAO 완성되면 주석 제거해야 됨~~~~!
+				if("로그인 성공".equals(dao.result)) {
+					oos_server.writeObject(
+				} else if("비밀번호를 확인해주세요.".equals(dao.result)) {
+					
+				} else if("존재하지 않는 아이디입니다.".equals()) {
+					
+				} else {
+					System.out.println("프로시저 수정????!!!!");
+				}
+				*/
+			} catch (ClassNotFoundException cnfe) {
+				cnfe.printStackTrace();
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
 			}
 		}
 	}
--- KosmoSketch/src/test/project1/Protocol.java
+++ KosmoSketch/src/test/project1/Protocol.java
@@ -1,18 +1,25 @@
 package test.project1;
 
 public class Protocol {
-	public final static int	_CLIENT_INFO	= 100;
-	public final static int	_READY			= 201;
-	public final static int	_NOT_READY		= 202;
-	public final static int	_START			= 203;
-	public final static int	_DAP			= 301;
-	public final static int	_CHAT			= 302;
-	public final static int	_CORRECT		= 303;
-	public final static int	_OVER_MEM		= 400;
-	public final static int	_PAINT			= 501;
-	public final static int	_ERASE			= 502;
-	public final static int	_EXIT			= 600;
+	public final static String	_CUT			= "#";
 
-	public static final int _MAKEROOM 		= 1001;
-	public static final int	_ROOMIN			= 1002;
+	public final static int		_LOGIN			= 1;
+	public final static int		_LOGIN_SUCCESS	= 2;
+	public final static int		_ID_FAILURE		= 3;
+	public final static int		_PW_FAILURE		= 4;
+
+	public final static int		_CLIENT_INFO	= 100;
+	public final static int		_READY			= 201;
+	public final static int		_NOT_READY		= 202;
+	public final static int		_START			= 203;
+	public final static int		_DAP			= 301;
+	public final static int		_CHAT			= 302;
+	public final static int		_CORRECT		= 303;
+	public final static int		_OVER_MEM		= 400;
+	public final static int		_PAINT			= 501;
+	public final static int		_ERASE			= 502;
+	public final static int		_EXIT			= 600;
+
+	public static final int		_MAKEROOM		= 1001;
+	public static final int		_ROOMIN			= 1002;
 }
