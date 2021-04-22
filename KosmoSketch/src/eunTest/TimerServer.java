b3d9a89f9238a75b22fc9e78cf4c1d98770b45b5
naseokw <na_0590@outlook.kr>
2021-04-22 21:59:03 +0900

    04/22 21:58 nsw

--- KosmoSketch/src/eunTest/TimerServer.java
+++ KosmoSketch/src/eunTest/TimerServer.java
@@ -1,34 +1,63 @@
 package eunTest;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import eunTest.Port;
+public class TimerServer extends ServerSocket implements Runnable {
 
-public class TimerServer extends ServerSocket implements Runnable{
-	Socket client = null;
-	Thread timerThread = null;
-	int portNum = 0;
-	public TimerServer(int port) throws IOException {
-		super(port);
-		portNum = port;
-		timerThread = new Thread(this);
-		timerThread.start();
+	Socket client 		= null;
+	Thread timerThread 	= null;
+	int portNum 		= 0;
+	boolean isStop = false;
+
+	int remainTime;
+	String timer;
+	boolean isGaming 	= false;
+
+	ObjectOutputStream oos 	= null;
+	TimerServerThread tst 	= null;
+
+	public TimerServer() throws IOException {
+		super();
 	}
-	@Override
-	public void run() {
-		boolean isStop = false;
-		try {
-			while(!isStop) {
-				client = this.accept();				
+
+	Thread timeCountingThread = new Thread(this) {//시간만 세는 스레드 0초가되면 while문 스탑시키고  59초로 초기화후 사망
+		@Override
+		public void run() {
+			boolean isStop = false;
+			try {
+				remainTime = 59;
+				while (!isStop) {
+					timer = String.format("00:00:%02d", remainTime);
+					remainTime--;  
+					Thread.sleep(1000);
+					if (remainTime == 0) {
+						isStop = true;
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
-		catch (IOException e) {
-			// TODO Auto-generated catch block
+	};
+
+	@Override
+	public void run() {// 연결해오는 클라받아서 서버스레드 할당해주기
+		try {
+			while (!isStop) {
+				client = this.accept();
+				tst = new TimerServerThread(client, this);
+				tst.start();
+				while (isGaming) {
+					timeCountingThread.start();
+				}
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
--- KosmoSketch/src/eunTest/TimerServerThread.java
+++ KosmoSketch/src/eunTest/TimerServerThread.java
@@ -1,11 +1,39 @@
 package eunTest;
 
-public class TimerServerThread extends Thread {
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+
+
+public class TimerServerThread extends Thread {//시간받아서 담당클라로 토스
+	
+	TimerServer ts = null;
+	Socket client = null;
+	boolean isGameEnd = false;;
+	
+	ObjectOutputStream oos = null;
+	
+
+	
 	public TimerServerThread() {
 		this.start();
 	}
+	
+	public TimerServerThread(Socket client, TimerServer ts) {
+		this.client = client;
+		this.ts = ts;
+		
+	}
 	@Override
 	public void run() {
-		
+		try {
+			oos = new ObjectOutputStream(client.getOutputStream());
+			
+			while(!isGameEnd) {
+				oos.writeObject(ts.timer);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
